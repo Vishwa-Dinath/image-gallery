873d5a5b8356bf9b4188c3c51fa72f8f2efe23b8
VishwaDinath <vishwadinath97@gmail.com>
2023-06-03 20:43:50 +0530

    Initial commit- Project Structure was created

---
+++ front-end/src/js/main.js
@@ -0,0 +1,14 @@
+const btnUpload = $('#btn-upload');
+const overlayElm = $('#overlay');
+const bodyElm = $('body');
+
+btnUpload.on('click',()=>{
+    overlayElm.removeClass("d-none");
+});
+bodyElm.on('keydown',(eventData)=>{
+    console.log(eventData.code);
+    if (eventData.code==='Escape' && !overlayElm.hasClass('d-none')) overlayElm.addClass('d-none');
+});
+overlayElm.on('click',(eventData)=>{
+    if (eventData.target===overlayElm[0]) overlayElm.addClass('d-none');
+});
